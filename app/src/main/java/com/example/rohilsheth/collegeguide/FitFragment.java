c58db2453594d8032176ce0020b4acd6a62af278
Rohil Sheth <rohilsheth@gmail.com>
2018-04-27 08:27:55 -0400

    Add Fragments, layout

---
+++ app/src/main/java/com/example/rohilsheth/collegeguide/FitFragment.java
@@ -0,0 +1,10 @@
+package com.example.rohilsheth.collegeguide;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rohilsheth on 4/27/18.
+ */
+
+public class FitFragment extends Fragment {
+}
